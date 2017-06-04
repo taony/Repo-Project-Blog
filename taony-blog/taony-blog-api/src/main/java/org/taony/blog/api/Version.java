9f52b918c2a0633ebe28704ef04ceaab0065d782
taony <taony@users.noreply.github.com>
2017-06-04 16:46:23 +0800

    create

---
+++ taony-blog/taony-blog-api/src/main/java/org/taony/blog/api/Version.java
@@ -0,0 +1,6 @@
+package org.taony.blog.api;
+
+public class Version {
+
+	public static final String NAME = "TAONY BOLOG";
+}
---
+++ taony-blog/taony-blog-web/src/main/java/com/taony/blog/web/App.java
@@ -0,0 +1,12 @@
+package com.taony.blog.web;
+
+import org.taony.blog.api.Version;
+
+public class App {
+
+	public static void main(String args[]) {
+
+		System.out.println(Version.NAME);
+
+	}
+}
